2db2285906837a4fc32e516bb4913e5cee635106
tbellicha <tanguy.bellicha@epitech.eu>
2024-03-09 20:58:03 -0800

    [ADD] Kotlin code

---
+++ Java_Kotlin_Clojure/Kotlin/HelloWorld.kt
@@ -0,0 +1,30 @@
+fun execForLoop() {
+    val classes = arrayOf("CS 351", "CS 446")
+    println("Here are the best classes at CSUSM:")
+    for (s in classes) {
+        val result = "- " + s
+        println(result)
+    }
+}
+
+fun execWhileLoop() {
+    var i = 1
+    while (i <= 10) {
+        println(Math.pow(i++.toDouble(), 2.0))
+    }
+}
+
+fun execDoWhileLoop() {
+    var i = 1.0
+    do {
+        println(Math.pow(2.0, i++))
+        i++
+    } while (i <= 10)
+}
+
+
+fun main() {
+    execForLoop()
+    execWhileLoop()
+    execDoWhileLoop()
+}
